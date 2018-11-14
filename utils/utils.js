787e9da78657b2876e76f9dc397e95544f4ce2d4
tugudush <jerome2kph@gmail.com>
2018-11-14 13:54:01 +0800

    assertion library

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,14 @@
 var add = (a, b) => a + b;
 var square = (a) => a * a;
+var setName = (user, fullName) => {
+  var names = fullName.split(' ');
+  user.firstName = names[0];
+  user.lastName = names[1];
+  return user;
+};
 
 module.exports = {
   add,
-  square
-}
+  square,
+  setName
+};
