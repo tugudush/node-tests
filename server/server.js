74f64b2f3735ba655bc07bb6dcf903fe50416968
tugudush <jerome2kph@gmail.com>
2018-11-20 14:06:11 +0800

    404 test

--- server/server.js
+++ server/server.js
@@ -4,9 +4,16 @@ var app = express();
 var port = 3000;
 
 app.get('/', function(req, res) {
-  res.send('Hello world!');
+  //res.status(200).send('Hello world!');
+  //res.send('Hello world!');
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0'
+  });
 }); // End of app.get('/', function(req, res) {
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`)
 });
+
+module.exports.app = app;
---
+++ server/server.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const expect = require('expect');
+var app = require('./server').app;
+
+it('should return hello world response', (done) => {
+  request(app)
+  .get('/')
+  .expect(404)
+  .expect((res) => {
+    expect(res.body).toInclude({
+      error: 'Page not found.'
+    }) // End of expect(res.body).toInclude({
+  }) // End of .expect((res) => {
+  .end(done);
+});
