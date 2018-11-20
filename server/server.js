21bb8ff1e48dd1b362362963649f6b0be8aa0d83
tugudush <jerome2kph@gmail.com>
2018-11-20 18:20:46 +0800

    nodejs express test part 2

--- server/server.js
+++ server/server.js
@@ -3,17 +3,33 @@ const express = require('express');
 var app = express();
 var port = 3000;
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   //res.status(200).send('Hello world!');
   //res.send('Hello world!');
   res.status(404).send({
     error: 'Page not found.',
     name: 'Todo App v1.0'
-  });
-}); // End of app.get('/', function(req, res) {
+  }); // End of res.status(404).send({
+}); // End of app.get('/', (req, res) => {
 
 // GET /users
 // Give users a name prop and age prop
+app.get('/users', (req, res) => {
+  res.status(200).send([
+    {
+      name: 'Mike',
+      age: 27
+    },
+    {
+      name: 'Andrew',
+      age: 25
+    },
+    {
+      name: 'Jen',
+      age: 26
+    }
+  ]); // End of res.status(200).send({
+}); // End of app.get('/users', (req, res) => {
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`)
--- server/server.test.js
+++ server/server.test.js
@@ -9,7 +9,7 @@ it('should return hello world response', (done) => {
   .expect((res) => {
     expect(res.body).toInclude({
       error: 'Page not found.'
-    }) // End of expect(res.body).toInclude({
+    }); // End of expect(res.body).toInclude({
   }) // End of .expect((res) => {
   .end(done);
 });
@@ -17,3 +17,16 @@ it('should return hello world response', (done) => {
 // Make a new test
 // assert 200
 // Assert that you exist in users array
+
+it('should return users', (done) => {
+  request(app)
+  .get('/users')
+  .expect(200)
+  .expect((res) => {
+    expect(res.body).toInclude({
+      name: 'Andrew',
+      age: 25
+    });
+  }) // End of .expect((res) => {
+  .end(done);
+}); // End of it('should return users', (done) => {
