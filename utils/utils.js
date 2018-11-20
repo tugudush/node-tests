65dab6188c031c24514fb2d8e8e69cd3f47dd89b
tugudush <jerome2kph@gmail.com>
2018-11-20 12:11:44 +0800

    test async

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,19 @@
 var add = (a, b) => a + b;
+
+var asyncAdd = (a, b, callback) => {
+  setTimeout(function() {
+    callback(a + b);
+  }, 1000); // End of setTimeout(function() {
+}; // End of var asyncAdd = (a, b, callback) => {
+
 var square = (a) => a * a;
+
+var asyncSquare = (a, callback) => {
+  setTimeout(function() {
+    callback(a * a);
+  }, 1000); // End of setTimeout(function() {
+}; // End of var asyncSquare = (a, b, callback) => {
+
 var setName = (user, fullName) => {
   var names = fullName.split(' ');
   user.firstName = names[0];
@@ -9,6 +23,8 @@ var setName = (user, fullName) => {
 
 module.exports = {
   add,
+  asyncAdd,
   square,
+  asyncSquare,
   setName
 };
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -4,15 +4,27 @@ const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
-
   expect(res).toBe(44).toBeA('number');
-});
+}); // End of it('should add two numbers', () => {
+
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    expect(sum).toBe(7).toBeA('number');
+    done();
+  });  
+}); // End of it('should async add two numbers', () => {
 
 it('should square a number', () => {
   var res = utils.square(3);
-
   expect(res).toBe(9).toBeA('number');
-});
+}); // End of it('should square a number', () => {
+
+it('should asycn square a number', (done) => {
+  utils.asyncSquare(3, (res) => {
+    expect(res).toBe(9).toBeA('number');
+    done();
+  });
+}); // End of it('should asycn square a number', (done) => {
 
 // should verify first and last names are set
 // assert it includes firstName and lastName with proper values
@@ -24,7 +36,7 @@ it('should set firstName and lastName', () => {
     firstName: 'Andrew',
     lastName: 'Mead'
   });
-});
+}); // End of it('should set firstName and lastName', () => {
 
 // it('should expect some values', () => {
 //   // expect(12).toNotBe(12);
